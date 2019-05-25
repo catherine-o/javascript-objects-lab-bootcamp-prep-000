b5131f958a8f250ba416cda732f55a9b7e291149
Catherine O'Hara <catherine.ohara@outlook.com>
2019-05-25 03:37:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
+  
   return object;
 }
 
 function deleteFromObjectByKey(object, key) {
+  delete object[key];
   
+  return object;
 }
